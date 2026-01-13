08701bd3748c8ef71133d2e786729e6459039e12
Lignuz <lignuz@gmail.com>
2026-01-13 17:35:11 +0900

    Real-ESRGAN-x4plus.onnx 모델 링크 수정

--- download_models.ps1
+++ download_models.ps1
@@ -94,7 +94,7 @@ $models = @(
     @{ Name = "mobile_sam.decoder.onnx";          Url = "https://huggingface.co/Acly/MobileSAM/resolve/main/sam_mask_decoder_multi.onnx?download=true" },
     @{ Name = "sam2_hiera_small.encoder.onnx";    Url = "https://huggingface.co/vietanhdev/segment-anything-2-onnx-models/resolve/main/sam2_hiera_small.encoder.onnx?download=true" },
     @{ Name = "sam2_hiera_small.decoder.onnx";    Url = "https://huggingface.co/vietanhdev/segment-anything-2-onnx-models/resolve/main/sam2_hiera_small.decoder.onnx?download=true" },
-    @{ Name = "Real-ESRGAN-x4plus.onnx";          Url = "https://huggingface.co/qualcomm/Real-ESRGAN-x4plus/resolve/83db0da6e1b4969f85fe60ee713bd2c2b3160c23/Real-ESRGAN-x4plus.onnx?download=true" },
+    @{ Name = "Real-ESRGAN-x4plus.onnx";          Url = "https://huggingface.co/qualcomm/Real-ESRGAN-x4plus/resolve/01179a4da7bf5ac91faca650e6afbf282ac93933/Real-ESRGAN-x4plus.onnx" },
     @{ Name = "rmbg-1.4.onnx";                    Url = "https://huggingface.co/briaai/RMBG-1.4/resolve/main/onnx/model.onnx?download=true" },
 	@{ Name = "version-RFB-320.onnx";             Url = "https://github.com/Linzaer/Ultra-Light-Fast-Generic-Face-Detector-1MB/raw/master/models/onnx/version-RFB-320.onnx?download=true" },
 	@{ Name = "yolov8n-face.onnx";                Url = "https://huggingface.co/deepghs/yolo-face/resolve/main/yolov8n-face/model.onnx?download=true" },
